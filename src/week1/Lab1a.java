adaeb915e02f09e9fa31bb161b2aa0309fa0c3a1
Miguel Elizalde <miguel.elizalde.19.98@live.com>
2023-01-04 09:32:19 -0700

    week1 Prep

---
+++ src/week1/Lab1a.java
@@ -0,0 +1,57 @@
+/**
+* File: Lab1a.java
+* Description: Java Basics Review
+* Lessons Learned: This helped me to review what I've learned last semester.
+* Instructor's Name: Barbara Chamberlin
+*
+* @author: Miguel Elizalde
+* @since: 01/02/2022
+*/
+
+package week1;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Lab1a {
+    public static void main(String[] args) {
+
+        Scanner sIn = new Scanner(System.in);
+        boolean dW1 = true;
+        ArrayList<Integer> numList = new ArrayList<Integer>();
+        String input;
+
+
+        do {
+            System.out.print("\nPlease enter a number (or hit the <Enter> key to stop): ");
+            input = sIn.nextLine();
+            if (!"".equals(input)) {
+                try {
+                    numList.add(Integer.parseInt(input));
+                } catch (Exception e) {
+                    System.out.println("Invalid response.  '"+input+"' is not a number.");
+                }
+            } else {
+                dW1 = false;
+            }
+        } while (dW1);
+
+        int sum = addNumbers(numList);
+        System.out.println("The total of the list of numbers is: "+sum);
+
+        int largest = largestNumber(numList);
+        System.out.println("The biggest number in the list is: "+largest);
+        // Scanner Close
+        sIn.close();
+    }
+
+    public static int addNumbers(ArrayList<Integer> numList) {
+
+        return 0;
+    }
+
+    public static int largestNumber(ArrayList<Integer> numList) {
+
+        return 0;
+    }
+}
